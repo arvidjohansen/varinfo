d27f3c69fed432423ec77e570284ae4a64176918
Biskus <biskus@gmail.com>
2021-03-24 19:21:28 +0100

    Upgraded to version 0.6.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='varinfo',
     packages=['varinfo'],
     description='Support functions for printing human readable info about objects',
-    version='0.5',
+    version='0.6.0',
     url='https://github.com/arvidjohansen/varinfo',
     author='Arvid Johansen',
     author_email='arvidj89@gmail.com',
